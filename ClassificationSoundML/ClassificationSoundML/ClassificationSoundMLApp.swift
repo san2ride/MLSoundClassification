f174c679a700d8a61dbdd230db30a19b86b91881
Jason <sanchezjason@me.com>
2024-02-06 15:56:41 -0700

    swiftui/machine_learning/sound_classification-ClassificationSoundMLView

---
+++ ClassificationSoundML/ClassificationSoundML/ClassificationSoundMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassificationSoundMLApp.swift
+//  ClassificationSoundML
+//
+//  Created by don't touch me on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ClassificationSoundMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
